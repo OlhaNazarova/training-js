f2a547ecde130f87fa655bb7bda5a224019909f8
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2024-02-21 21:30:31 +0200

    module 4 process

---
+++ js/task-4.js
@@ -0,0 +1,35 @@
+// ------------------------------------------------------ap2-------
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     owner: {
+//         name: 'Henry',
+//         phone: '063-21-19-30',
+//         email: 'henry.carter@aptmail.com',
+//     },
+//   };
+//   console.log(apartment);
+
+//   -----------------------------------------------------ap3------
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"],
+  };
+  
+  // Change code below this line
+  const aptRating = apartment.rating;
+  const aptDescr = apartment.descr;
+  const aptPrice = apartment.price;
+  const aptTags = apartment.tags;
+  // Change code above this line
+  console.log(aptRating);
+  console.log(aptDescr);
+  console.log(aptPrice);
+  console.log(aptTags);
+  
